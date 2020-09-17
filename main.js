f10e5c12385f3b54af51e2f1cb51db86e7839f78
zg.michalak <zg.michalak@gmail.com>
2020-09-17 14:49:22 +0200

    first time commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0
+}
+
+const game = {
+    playerHand: "",
+    aiHand: "",
+    playerHandHTML: "",
+}
+
+const hands = [...document.querySelectorAll('.select img')];
+
+//Pierwsza funkcja
+function handSelection() {
+    console.log(this);
+    game.playerHand = this.dataset.option
+}
+
+hands.forEach(hand => hand.addEventListener("click", handSelection));
