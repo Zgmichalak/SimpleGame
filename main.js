a1fdff6912a95f3e766c1d15a723ee39f6ffe6cf
zg.michalak <zg.michalak@gmail.com>
2020-09-18 09:06:09 +0200

    End game function added

--- main.js
+++ main.js
@@ -7,16 +7,70 @@ const gameSummary = {
 
 const game = {
     playerHand: "",
-    aiHand: "",
-    playerHandHTML: "",
+    aiHand: ""
 }
 
 const hands = [...document.querySelectorAll('.select img')];
 
-//Pierwsza funkcja
+//Pierwsza funkcja - wybor opcji
 function handSelection() {
-    console.log(this);
     game.playerHand = this.dataset.option
+    hands.forEach(hand => hand.style.boxShadow = `` );
+    this.style.boxShadow = `0 0 0 4px lightblue`;
 }
 
 hands.forEach(hand => hand.addEventListener("click", handSelection));
+
+//Druga funkcja - sterujaca
+function aiChoice() {
+    const aiHand = hands[Math.floor(Math.random()*3)].dataset.option;
+    return aiHand;
+}
+
+function checkResult(player, ai) {
+if(player === ai) {
+    return 'draw'
+} else if((player === "papier" && ai === "kamien") || (player === "kamien" && ai === "nozyczki") ||(player === "nozyczki" && ai === "papier")) {
+return 'win'
+} else { return 'loss';
+}
+}
+
+function publishResult(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+    gameSummary.numbers++;
+    document.querySelector('p.numbers span').textContent = gameSummary.numbers;
+
+    if(result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = "Wygrana byczku!";
+        document.querySelector('[data-summary="who-win"]').style.color = "green";
+    } else if(result === "loss") {
+        document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+        document.querySelector('[data-summary="who-win"]').textContent = "Jestes przegrywem zyciowym!";
+        document.querySelector('[data-summary="who-win"]').style.color = "red";
+    } else {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = "Remis cwaniaczku!";
+        document.querySelector('[data-summary="who-win"]').style.color = "purple";
+    }
+}
+
+function endGame() {
+    document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = "";
+    game.playerHand = "";
+    aiHand = "";
+}
+
+function startGame() {
+    if(!game.playerHand) {
+        return alert('Wybierz jedna z trzech opcji - papier, kamien lub nozyce!');
+    }
+    game.aiHand = aiChoice()
+    const gameResult = checkResult(game.playerHand, game.aiHand);
+    publishResult(game.playerHand, game.aiHand, gameResult);
+    endGame();
+}
+
+document.querySelector('.start').addEventListener('click', startGame);
